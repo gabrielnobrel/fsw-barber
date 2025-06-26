3533423073117a4489509377d39c96541c598404
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-06-26 14:52:21 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import { Button } from "./_components/ui/button"
 const Home = () => {
   return (
     <>
-      <div className="flex h-screen items-center justify-center">
-        <h1 className="text-4xl font-bold">Welcome to FSW Barber!</h1>
-
-        <Button>Click Me</Button>
-      </div>
+      <Button>Click Me</Button>
     </>
   )
 }
