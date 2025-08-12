0ab37eac638e7ce51b34188e78828f939a290c6a
gabrielnobrel <gabriel_nobresantos@hotmail.com>
2025-08-12 14:11:31 -0300

    feat: update welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -44,8 +46,18 @@ const Home = async () => {
     <>
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Gabriel!</h2>
-        <p>Quarta-feira, 02 de Julho</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "Bem-vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd ", { locale: ptBR })}
+          </span>
+          <span>de</span>
+          <span className="capitalize">
+            {format(new Date(), " MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         {/* Busca */}
         <div className="mt-6">
